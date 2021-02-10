0ba493e70f317dad6f59e8ae3f98217c46e66ab0
Varin <zvv1287@mail.ru>
2021-02-10 18:21:55 +0300

    Добавленна регистрация пользователя

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from adminapp.views import index
+from adminapp.views import index, admins_user_read, admins_user_create
 
 
 app_name = 'adminapp'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('admins-user-read/', admins_user_read, name='admins_user_read'),
+    path('admins-user-create/', admins_user_create, name='admins_user_create'),
 ]
 
 
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+from authapp.models import User
+from adminapp.forms import AdminUserRegisterForm
 
 def index(request):
-
     return render(request, 'adminapp/index.html')
+
+def admins_user_read(request):
+    context = {'users': User.objects.all()}
+    return render(request, 'adminapp/admins_user_read.html', context)
+
+def admins_user_create(request):
+    if request.method == 'POST':
+        form = AdminUserRegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admins_user_read'))
+    else:
+        form = AdminUserRegisterForm()
+    context = {'form': form, 'head': 'регистрация'}
+    return render(request, 'adminapp/admin-users-create.html', context)
 
